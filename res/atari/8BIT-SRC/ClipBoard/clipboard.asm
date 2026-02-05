6d0decfb2709e02fa3fca7525617c03c09daaf7e
paul <paul@localhost.localdomain>
2026-02-05 07:24:21 -0500

    Add Y: device

--- res/atari/8BIT-SRC/ClipBoard/clipboard.asm
+++ res/atari/8BIT-SRC/ClipBoard/clipboard.asm
@@ -1,64 +1,58 @@
- ; =============================================================
+;; =============================================================
 ;  PC Clipboard Driver (Device "Y:") for AspeQt
-;  VERSION 5: Fixed "Keyboard Crash" & Display Bugs
-;  Usage: ENTER "Y:" to paste from PC
+;  VERSION 8: Page 6 Static Storage (Crash Proof)
+;  Usage: ENTER "Y:"
 ; =============================================================
 
-    icl 'menu_sym.asm'  ; Defines Temp1 ($80), Temp2, etc.
+    icl 'menu_sym.asm'
 
-    org $4000           ; Standard executable start
+    org $4000
 
 ; --- Local Equates ---
-MEMLO   = $02E7     ; Pointer to available user memory
-HATABS  = $031A     ; Handler Table
+MEMLO   = $02E7     
+HATABS  = $031A     
 CIO_OK  = 1
-CIO_EOF = 136       ; Error 136 (End Of File)
+CIO_EOF = 136
+
+; --- STATIC STORAGE (PAGE 6) ---
+; We use Page 6 to avoid complex relocation patching.
+; This memory is safe and fixed.
+PG6_BUFFER  = $0600     ; 128 Bytes for SIO Data
+PG6_PTR     = $0680     ; 1 Byte: Buffer Index
+PG6_LEN     = $0681     ; 1 Byte: Buffer Length
 
 ; =============================================================
-;  INSTALLATION PROGRAM (Runs once, then exits)
+;  INSTALLATION ROUTINE
 ; =============================================================
 Start:
-    ; 1. Print Banner
     jsr Printf
     .byte 155,'Installing Y: Clipboard Driver...',155,0
 
-    ; 2. Prepare Pointers for Relocation
-    ; Temp1 ($80) = Destination (Current MEMLO)
+    ; 1. Prepare Pointers for Copy
     lda MEMLO
     sta Temp1
     lda MEMLO+1
     sta Temp1+1
     
-    ; Temp2 ($82) = Driver Size
     lda #< (DriverEnd - DriverStart)
     sta Temp2
     lda #> (DriverEnd - DriverStart)
     sta Temp2+1
 
-    ; 3. Copy Driver to MEMLO
+    ; 2. Copy Driver Code to MEMLO
     ldy #0
 CopyLoop:
     lda DriverStart,y
-    sta (Temp1),y       ; Valid ZP Indirect
+    sta (Temp1),y
     iny
     bne CheckSize
-    inc DriverStart+1   ; Bump Source High
-    inc Temp1+1         ; Bump Dest High
+    inc DriverStart+1
+    inc Temp1+1
 CheckSize:
     cpy Temp2
     bne CopyLoop
 
-    ; 4. Relocation Patching (The Math)
-    ; We must shift all JMP targets by the offset
-    ; Offset = MEMLO - CompileStart ($4000)
-    
-    ; Reset Temp1 to point to the New Driver Start (MEMLO)
-    lda MEMLO
-    sta Temp1
-    lda MEMLO+1
-    sta Temp1+1
-
-    ; Calculate Offset
+    ; 3. Calculate Global Offset (New - Old)
     lda MEMLO
     sec
     sbc #<DriverStart
@@ -67,65 +61,37 @@ CheckSize:
     sbc #>DriverStart
     sta Temp3+1         ; Offset High
 
-    ; A. Patch the Jump Table (First 12 bytes)
+    ; 4. Patch Jump Table (Offsets only)
+    ; Reset Temp1 to New Driver Start
+    lda MEMLO
+    sta Temp1
+    lda MEMLO+1
+    sta Temp1+1
+
     ldy #0
 PatchTable:
     clc
-    lda (Temp1),y       ; Load Low Byte from RAM
-    adc Temp3           ; Add Offset Low
-    sta (Temp1),y       ; Save back
+    lda (Temp1),y       ; Load Low
+    adc Temp3           ; Add Offset
+    sta (Temp1),y       ; Store New Low
     iny
     
-    lda (Temp1),y       ; Load High Byte
-    adc Temp3+1         ; Add Offset High
-    sta (Temp1),y       ; Save back
+    lda (Temp1),y       ; Load High
+    adc Temp3+1         ; Add Offset
+    sta (Temp1),y       ; Store New High
     iny
     
-    cpy #12             ; 6 vectors * 2 bytes
+    cpy #12             ; 6 Vectors * 2 Bytes
     bne PatchTable
     
-    ; B. Patch the SIO Buffer Pointer
-    ; We need to find the instructions "LDA #<Buffer" inside the copy.
-    ; Location = MEMLO + (LOC_BufLo - DriverStart)
-    
-    ; Patch Low Byte Instruction
-    clc
-    lda MEMLO
-    adc #< (LOC_BufLo + 1 - DriverStart) 
-    sta Temp1
-    lda MEMLO+1
-    adc #> (LOC_BufLo + 1 - DriverStart)
-    sta Temp1+1
-    
-    ; Calculate New Buffer Address Low
-    clc
-    lda #<Buffer
-    adc Temp3           
-    ldy #0
-    sta (Temp1),y       ; Patch the operand
+    ; Note: No other patching is needed because variables are at fixed $0600!
 
-    ; Patch High Byte Instruction
-    clc
-    lda MEMLO
-    adc #< (LOC_BufHi + 1 - DriverStart)
-    sta Temp1
-    lda MEMLO+1
-    adc #> (LOC_BufHi + 1 - DriverStart)
-    sta Temp1+1
-    
-    ; Calculate New Buffer Address High
-    clc
-    lda #>Buffer
-    adc Temp3+1         
-    ldy #0
-    sta (Temp1),y       ; Patch the operand
-
-    ; 5. Hook into HATABS (Using 'Y' now!)
+    ; 5. Install in HATABS
     ldx #0
 FindSlot:
     lda HATABS,x
     beq FoundSlot
-    cmp #'Y'        ; Check for existing Y: handler
+    cmp #'Y'
     beq FoundSlot
     inx
     inx
@@ -133,20 +99,19 @@ FindSlot:
     cpx #35
     bne FindSlot
     
-    ; Table Full Error
     jsr Printf
     .byte 155,'Error: HATABS Full!',155,0
     rts
 
 FoundSlot:
-    lda #'Y'        ; Register Device 'Y'
+    lda #'Y'
     sta HATABS,x
     lda MEMLO
-    sta HATABS+1,x  ; Point to New Driver
+    sta HATABS+1,x
     lda MEMLO+1
     sta HATABS+2,x
 
-    ; 6. Reserve Memory (Update MEMLO)
+    ; 6. Reserve Memory
     clc
     lda MEMLO
     adc Temp2
@@ -155,17 +120,21 @@ FoundSlot:
     adc #0
     sta MEMLO+1
 
-    ; 7. Print Success
+    ; 7. Initialize Variables
+    lda #0
+    sta PG6_PTR
+    sta PG6_LEN
+
     jsr Printf
     .byte 'Success! Type: ENTER "Y:"',155,0
     rts
 
 ; =============================================================
-;  RESIDENT DRIVER CODE (The Template)
+;  RESIDENT DRIVER CODE
 ; =============================================================
 DriverStart:
 
-    ; Jump Table (Patched at runtime)
+    ; Jump Table
     .word H_OPEN-1
     .word H_CLOSE-1
     .word H_GETBYTE-1
@@ -174,70 +143,104 @@ DriverStart:
     .word H_SPECIAL-1
     jmp H_INIT      
 
-    ; Variables
-BufPtr      .byte 0     
-BufLen      .byte 0     
-Buffer      .ds 128     
-
+; --- OPEN HANDLER ---
 H_OPEN:
-    lda #$4F        ; 'O'
-    ldx #0
-    ldy #0
-    jsr CallSIO
+    ; Use Stack to Save X (No variable needed)
+    txa
+    pha
+
+    lda #$4F        ; 'O' Open
+    jsr CallSIO     ; Returns N flag on error
     bmi OpenError
 
     lda #0
-    sta BufLen
-    sta BufPtr
+    sta PG6_LEN     ; Reset Static Vars
+    sta PG6_PTR
+    
+    pla             ; Restore X
+    tax
     ldy #CIO_OK
     rts
+
 OpenError:
+    pla
+    tax
     ldy #144        ; Device Error
     rts
 
+; --- GET BYTE HANDLER ---
 H_GETBYTE:
-    ldx BufPtr
-    cpx BufLen
+    txa             ; Save X
+    pha
+
+    ldx PG6_PTR
+    cpx PG6_LEN
     bne FetchChar
 
     jsr RefillBuffer
     bmi ReturnEOF
     
     ldx #0
-    stx BufPtr
+    stx PG6_PTR
+
 FetchChar:
-    ldx BufPtr
-    lda Buffer,x
-    inc BufPtr
+    ldx PG6_PTR
+    lda PG6_BUFFER,x   ; Read from Page 6
+    inc PG6_PTR
+    
+    ; Restore X, but preserve A (the character)
+    tax             ; Move Char to X temp
+    pla             ; Get Old X
+    tay             ; Move Old X to Y temp
+    txa             ; Move Char back to A
+    tya             ; Move Old X back to A? No, wait.
+    
+    ; Correct Restore Logic:
+    ; Stack: [Old X]
+    ; A = Char
+    
+    ; We need to return: A=Char, X=OldX
+    sta Temp1       ; Save Char to ZP temp (Safe in interrupt? Maybe not)
+                    ; Better: Use Y.
+    tay             ; Save Char in Y
+    pla             ; Get Old X
+    tax             ; Restore X
+    tya             ; Restore Char to A
+    
     ldy #CIO_OK
     rts
+
 ReturnEOF:
+    pla
+    tax
     ldy #CIO_EOF
     rts
 
+; --- REFILL BUFFER ---
 RefillBuffer:
-    lda #$52        ; 'R'
-    ldx #128
-    ldy #0
+    lda #$52        ; 'R' Read
     jsr CallSIO
     tya
     bmi RefillFail
+    
     lda #128
-    sta BufLen
+    sta PG6_LEN
     ldy #CIO_OK
     rts
 RefillFail:
     ldy #255
     rts
 
+; --- SIO WRAPPER ---
 CallSIO:
     sta DCOMND
-    stx DBYTLO
-    sty DBYTHI
+    lda #128        ; Always 128 bytes
+    sta DBYTLO
+    lda #0
+    sta DBYTHI
     
-    lda #$59        ; Device 'Y' (Fixed! Was $4B 'K')
+    lda #$59        ; Device 'Y'
     sta DDEVIC
-    
     lda #1
     sta DUNIT
     lda #$40        ; Receive
@@ -245,18 +248,16 @@ CallSIO:
     lda #15
     sta DTIMLO
     
-    ; Targets for Self-Modifying Code
-LOC_BufLo:
-    lda #<Buffer    
+    ; Point SIO to Fixed Page 6 Buffer
+    lda #<PG6_BUFFER
     sta DBUFLO
-LOC_BufHi:
-    lda #>Buffer    
+    lda #>PG6_BUFFER
     sta DBUFHI
     
     jsr SIOV
     rts
 
-; Stubs
+; --- STUBS ---
 H_CLOSE:
 H_PUTBYTE:
 H_STATUS:
@@ -270,9 +271,7 @@ DriverEnd:
 ; =============================================================
 ;  LIBRARIES
 ; =============================================================
-
-    icl 'printf.asm'    ; Uses your working print library
-    
+    icl 'printf.asm'
     run Start
     
     
